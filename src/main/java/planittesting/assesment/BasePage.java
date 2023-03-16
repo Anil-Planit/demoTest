d5fa9650975d4ed91220c18c4b0eb70a4ca10e19
anilkumars <anilkumars@PL-IN-PF466HD7.planit.local>
2023-03-16 14:46:08 +0530

    new Code

---
+++ src/main/java/planittesting/assesment/BasePage.java
@@ -0,0 +1,32 @@
+package planittesting.assesment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public abstract class BasePage {
+	
+	WebDriver driver;
+	
+	public BasePage(WebDriver driver)
+	{
+	this.driver=driver;
+	}
+	
+	
+	public void navigateTo(String categoryName, String subCategogyName, String productType) throws InterruptedException {
+		Thread.sleep(4000);
+		
+		WebElement category = driver.findElement(By.xpath("//nav[@class='navigation']//ul//span[text()='"+categoryName+"']"));
+		new Actions(driver).moveToElement(category);
+		
+		Thread.sleep(1000);
+		WebElement subCategory = driver.findElement(By.xpath("//*[@role=\"menu\"]//span[text()='"+subCategogyName+"']"));
+		new Actions(driver).moveToElement(subCategory);
+	
+		WebElement productLink = driver.findElement(By.xpath("//*[@role=\"menu\"]//span[text()='"+productType+"']"));
+		productLink.click();
+	}
+	
+}
